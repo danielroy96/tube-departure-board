24bc148197382e342546dcba0eb65be5f4bc6dae
Roy DY Mr (UG - Computer Science) <dr00115@surrey.ac.uk>
2019-07-10 21:12:02 +0100

    Spring initialised

---
+++ src/main/java/com/droy/tubedepartureboard/TubeDepartureBoardApplication.java
@@ -0,0 +1,13 @@
+package com.droy.tubedepartureboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TubeDepartureBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TubeDepartureBoardApplication.class, args);
+	}
+
+}
